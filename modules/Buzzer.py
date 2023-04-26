e32f32b1fc3c3777c8e6509efd8e753a1a82eb7c
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2023-04-26 16:52:55 -0600

    modulo para buzzer agregado

---
+++ modules/Buzzer.py
@@ -0,0 +1,15 @@
+from gpiozero import Buzzer
+
+
+class Alarm:
+    def __init__(self, pin=21) -> None:
+        self.buzzer = Buzzer(pin)
+
+    def beep(self, time=1, n=3) -> None:
+        """
+        Emite un sonido con el buzzer por el tiempo especificado y el numero de veces indicado
+
+        :param time: Tiempo en segundos que se emite el sonido, donde por defecto es 1 segundo
+        :param n: Numero de veces que se emite el sonido, donde por defecto es 3 veces
+        """
+        self.buzzer.blink(on_time=time, off_time=time, n=n)
